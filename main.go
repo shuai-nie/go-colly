f81f51f19d9363d2bca772b941e018d2c98080e3
Dave <178698695@qq.com>
2022-11-29 11:22:46 +0800

    pachong

--- main.go
+++ main.go
@@ -1,49 +1,64 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"github.com/PuerkitoBio/goquery"
 	"github.com/gocolly/colly"
+	"log"
+	"net/url"
+	"time"
 )
 
 func main(){
-	c := colly.NewCollector(colly.UserAgent("Opera/9.80 (Windows NT 6.1; U; zh-cn) Presto/2.9.168 Version/11.50"))
+	urlstr := "https://news.baidu.com"
+	u, err := url.Parse(urlstr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c := colly.NewCollector()
+
+	c.SetRequestTimeout(100 * time.Second)
+
+	c.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36"
 
 	c.OnRequest(func(r *colly.Request) {
-		r.Headers.Set("Host", "baidu.com")
+		// Request头部设定
+		r.Headers.Set("Host", u.Host)
 		r.Headers.Set("Connection", "keep-alive")
 		r.Headers.Set("Accept", "*/*")
-		r.Headers.Set("Origin", "")
-		r.Headers.Set("Referer", "http://www.baidu.com")
+		r.Headers.Set("Origin", u.Host)
+		r.Headers.Set("Referer", urlstr)
 		r.Headers.Set("Accept-Encoding", "gzip, deflate")
 		r.Headers.Set("Accept-Language", "zh-CN, zh;q=0.9")
-
-		fmt.Println("Visiting", r.URL)
 	})
-	
+
 	c.OnHTML("title", func(e *colly.HTMLElement) {
 		fmt.Println("title:", e.Text)
 	})
-	
-	c.OnHTML("body", func(e *colly.HTMLElement) {
-		e.ForEach(".hotnews a", func(i int, el *colly.HTMLElement) {
-			band := el.Attr("href")
-			title := el.Text
-			fmt.Printf("新闻 %d : %s - %s\n", i, title, band)
+
+	c.OnResponse(func(resp *colly.Response){
+		fmt.Println("response received", resp.StatusCode)
+
+		htmlDoc, err := goquery.NewDocumentFromReader(bytes.NewReader(resp.Body))
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		htmlDoc.Find(".hotnews a").Each(func(i int, s *goquery.Selection) {
+			band, _ := s.Attr("href")
+			title := s.Text()
+			fmt.Printf("热点新闻 %d: %s - $s\n", i, title, band)
+			c.Visit(band)
 		})
 	})
 
-	c.OnResponse(func(r *colly.Response) {
-		fmt.Println("response received", r.StatusCode)
-		// 设置context
-		// fmt.Println(r.Ctx.Get("url"))
+	c.OnError(func(resp *colly.Response, errHttp error) {
+		err = errHttp
 	})
 
-	// 对visit的线程数做限制，visit可以同时运行多个
-	c.Limit(&colly.LimitRule{
-		Parallelism: 2,
-		//Delay:      5 * time.Second,
-	})
-
-	c.Visit("http://news.baidu.com")
+	err = c.Visit(urlstr)
 	
 }
