a6e15421646c3155d73a018f09c4ddfdfd3be2a4
Dave <yesheng35@126.com>
2022-12-05 16:12:11 +0800

    asd

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 	"net/url"
 	"os"
 	"time"
+	"unicode"
 )
 
 func main() {
@@ -118,4 +119,96 @@ func largestPalindrome(n int) int {
 		}
 	}
 	return 0
+}
+
+// 给定一个段落 (paragraph) 和一个禁用单词列表 (banned)。返回出现次数最多，同时不在禁用列表中的单词。
+// https://blog.csdn.net/weixin_52001449/article/details/124233408
+func mostCommonWord(paragraph string, banned []string) string {
+	ban := map[string]bool{}
+	for _, s := range banned {
+		ban[s] = true
+	}
+	freq := map[string]int{}
+	maxFreq := 0
+	var word []byte
+	for i, n := 0, len(paragraph); i <= n; i++ {
+		if i < n && unicode.IsLetter(rune(paragraph[i])) {
+			word = append(word, byte(unicode.ToLower(rune(paragraph[i]))))
+		} else if word != nil {
+			s := string(word)
+			if !ban[s] {
+				freq[s]++
+				maxFreq = max(maxFreq, freq[s])
+			}
+			word = nil
+		}
+	}
+
+	for s, f := range freq {
+		if f == maxFreq {
+			return s
+		}
+	}
+	return ""
+}
+
+func max(a, b int) int {
+	if b > a {
+		return b
+	}
+	return a
+}
+
+// https://blog.csdn.net/weixin_52001449/article/details/124257115
+// 给你一个整数 n ，按字典序返回范围 [1, n] 内所有整数。
+// 输入：n = 13
+// 输出：[1,10,11,12,13,2,3,4,5,6,7,8,9]
+func lexicalOrder(n int) []int {
+	var list = make([]int, n)
+	last := 1
+	for i := 0; i < n; i++ {
+		list[i] = last
+		if last*10 <= n {
+			last = 10 * last
+			continue
+		} else {
+			if last == n {
+				last = last / 10
+			}
+
+			for last%10 == 9 {
+				last = last / 10
+			}
+		}
+		last++
+	}
+	return list
+}
+
+// 给你一个字符串 s 和一个字符 c ，且 c 是 s 中出现过的字符。
+//返回一个整数数组 answer ，其中 answer.length == s.length 且 answer[i] 是 s 中从下标 i 到离它 最近 的字符 c 的 距离 。
+//两个下标 i 和 j 之间的 距离 为 abs(i - j) ，其中 abs 是绝对值函数。
+// https://blog.csdn.net/weixin_52001449/article/details/124285363
+func shortestToChar(s string, c byte) []int {
+	n := len(s)
+	answer := make([]int, n)
+
+	index := -n
+	for i, ch := range s {
+		if byte(ch) == c {
+			index = i
+		}
+		answer[i] = i - index
+	}
+
+	index = 2 * n
+	for i := n - 1; i >= 0; i-- {
+		if s[i] == c {
+			index = i
+		}
+		if answer[i] > index-i {
+			answer[i] = index - i
+		}
+	}
+	return answer
 }
